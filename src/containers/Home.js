d5e0cbd46c55b422bc880749a6b34d25002dff87
sw-yx <swyx@swyx.io>
2018-11-03 15:27:10 -0700

    init

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,8 +3,11 @@ import { withSiteData } from 'react-static';
 //
 // import logoImg from '../logo.png';
 
+import netlifyIdentity from 'netlify-identity-widget';
+
 export default withSiteData(() => (
   <div style={{ textAlign: 'center' }}>
     <h1>Learn in Public</h1>
+    <button onClick={() => netlifyIdentity.open()}>skjdlskjd</button>
   </div>
 ));
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import netlifyIdentity from 'netlify-identity-widget';
 
 // Your top level component
-import App from './App'
+import App from './App';
 
 // Export your top level component as JSX (for static rendering)
-export default App
+export default App;
 
 // Render your app
 if (typeof document !== 'undefined') {
-  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
+  netlifyIdentity.init();
+
+  const renderMethod = module.hot
+    ? ReactDOM.render
+    : ReactDOM.hydrate || ReactDOM.render;
   const render = Comp => {
-    renderMethod(<Comp />, document.getElementById('root'))
-  }
+    renderMethod(<Comp />, document.getElementById('root'));
+  };
 
   // Render!
-  render(App)
+  render(App);
 }
