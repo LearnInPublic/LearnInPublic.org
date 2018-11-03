f421bac8eec70378656056a1347892c3b0b0d5dc
sw-yx <swyx@swyx.io>
2018-11-02 21:54:18 -0700

    init

---
+++ src/containers/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//
+
+export default () => (
+  <div>
+    <h1>This site is under construction.</h1>
+    <p>Sorry if you're seeing this, i'm just building this out</p>
+  </div>
+);
---
+++ src/containers/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withSiteData } from 'react-static';
+//
+// import logoImg from '../logo.png';
+
+export default withSiteData(() => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Learn in Public</h1>
+  </div>
+));
